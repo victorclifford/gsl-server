d1fcfba888a5464cae9f300b32d1830af599ea8a
victor clifford <victorclifford29@gmail.com>
2024-04-22 13:42:23 +0100

    cors fix

--- app.js
+++ app.js
@@ -17,7 +17,7 @@ const userRoutes = require("./routes/userRoutes");
 const origin = [
   "http://localhost:3000",
   "https://www.gosolar.ng",
-  "https://go-solar-frontend.vercel.app/",
+  "https://go-solar-frontend.vercel.app",
 ];
 const app = express();
 
